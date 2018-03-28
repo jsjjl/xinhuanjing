4c7af1fc6f2bb47f69e9cb867ef3a034b1ced4a6
jsjjl <jiangjinlei9@163.com>
2018-03-28 17:21:37 +0800

    3.28xw

--- views/user-list/user-list.vue
+++ views/user-list/user-list.vue
@@ -13,11 +13,12 @@
             
                 <Card>
                     <div style="margin:5px 0 20px;text-align: right;"> 
-                       
+                       <Input v-model="ssxm" placeholder="请输入姓名..." style="width: 150px"></Input>
+                       <Input v-model="sszh" placeholder="请输入账号..." style="width: 150px"></Input>
                      <Select v-model="model1" style="width:150px;text-align: left;">
                         <Option v-for="item in cityList" :value="item.value" :key="item.value">{{ item.label }}</Option>
                      </Select>
-                     <Button type="primary" @click="handleSearch3" >搜索</Button>
+                     <Button type="primary" @click="getData" >搜索</Button>
                       <Button type="primary" style="float:right;margin-left:10px;" @click="modal1 = true" >新增</Button>
 
                        <Modal
@@ -75,18 +76,86 @@
                             </Form>
 
                         </Modal>
+
+
+
+
+
+
+                           <Modal
+                            v-model="modal2"
+                            title="编辑"
+                            @on-ok="ok2"
+                            @on-cancel="cancel2">
+                           
+
+                            <Form :label-width="80">
+                                <FormItem label="姓名" >
+                                    <Input v-model="account2" />
+                                </FormItem>
+                                <FormItem label="手机号" >
+                                    <Input v-model="login_name2" />
+                                </FormItem>
+
+                                <FormItem label="职位" >
+                                    <Select  style="width:100%" v-model="login_type2" >
+                                        <Option v-for="item in articleStateList" :value="item.value" :key="item.value">{{ item.value }}</Option>
+                                    </Select>
+                                </FormItem>
+                                
+                                <FormItem label="身高cm" >
+                                    <Input v-model="height2" />
+                                </FormItem>
+                                <FormItem label="体重kg" >
+                                    <Input v-model="weight2" />
+                                </FormItem>
+                                <FormItem label="蓝牙名称" >
+                                    <Input v-model="blue_name2" />
+                                </FormItem>
+
+                                <FormItem label="蓝牙地址" >
+                                    <Input v-model="blue_address2" />
+                                </FormItem>
+
+                                <FormItem label="性别" >
+                                    <RadioGroup v-model="step2.pass">
+                                        <Radio :disabled="hasSubmit" label="男"></Radio>
+                                        <Radio :disabled="hasSubmit" label="女"></Radio>
+                                    </RadioGroup>
+                                </FormItem>
+
+                                <FormItem label="负责路段" >
+                                    <Input v-model="my_area2" />
+                                </FormItem>
+
+                                <FormItem label="公司">
+                                    <Select  style="width:100%" v-model="company2">
+                                        <Option v-for="item in companybox" :value="item.value" :key="item.value" >{{ item.value }}</Option>
+                                    </Select>
+                                </FormItem>
+                                
+                            </Form>
+
+                        </Modal>
    
                     </div>
                     <div class="" style=" margin-bottom: 20px;">
-                        <can-edit-table refs="table1" v-model="tableData" :columns-list="columnsList"  @on-delete="handleDel" ></can-edit-table>
+                        <!-- <can-edit-table refs="table1" v-model="tableData" :columns-list="columnsList"  @on-delete="handleDel" ></can-edit-table> -->
+
+                        <Table border :columns="columns7" :data="tableData"></Table>
                     </div>
-                   
+             
+                    <Page :total="pageTotal" :current="pageNum" :page-size="pageSize" show-elevator show-sizer show-total   
+placement="top" @on-change="handlePage" @on-page-size-change='handlePageSize'></Page>  
+
                 </Card>
             </Col>
         </Row>
 
     </div>
 </template>
+
+
 
 <script>
 import canEditTable from '../main-components/canEditTable.vue';
@@ -96,7 +165,34 @@ import Cookies from 'js-cookie';
 
 //引入axios
 import axios from 'axios';
+  var aa =Cookies.get('user_id');
+  console.log("cookies_user_id:",aa);
 
+
+var u=window.u||{};
+u.isArray=function(o) {
+  return typeof o=='object'&&Object.prototype.toString.call(o).slice(8,-1).toLowerCase()=='array';
+};
+/**
+ * 对json数据按照一定规则进行排列
+ * @param  {array} array [需要排序的数组]
+ * @param  {string} type  [排序时所依据的字段]
+ * @param  {boolean} asc   [可选参数，默认降序，设置为true即为升序]
+ * @return {none}       [无返回值]
+ */
+u.sort=function(array,type,asc) {
+  if(!u.isArray(array)) throw new Error('第一个参数必须是数组类型');
+  var asc=asc||false;
+  array.sort(function(a,b) {
+    if(!asc) {
+      return parseFloat(b[type])-parseFloat(a[type]);
+    } else {
+      return parseFloat(a[type])-parseFloat(b[type]);
+    }
+  });
+};
+
+var xgid="";
 
 export default {
     name: 'editable-table',
@@ -105,19 +201,13 @@ export default {
     },
     data () {
         return {
-            step: {
-                pass: '男'
-            },
-            account:'',
-            login_name:'',
-            height:'',
-            weight:'',
-            blue_name:'',
-            blue_address:'',
-            my_area:'',
-            companybox:[{value: '士明'}, {value: '馨靓'},  {value: '守利'},  {value: '冉然'},  {value: '彦宇'}],
-            modal1: false,
-            columnsList: [{
+            ssxm:'',
+            sszh:'',
+            pageTotal: 0,  
+            pageNum: 1,  
+            pageSize: 10,
+                 columns7: [
+                   {
     title: '序号',
     type: 'index',
     width: 80,
@@ -177,15 +267,111 @@ export default {
     editable: true
 },
 
-{
-    title: '操作',
-    align: 'center',
-    width: 190,
-    key: 'userid',
-    handle: ['edit', 'delete']
+                    {
+                        title: '操作',
+                        key: 'action',
+                        width: 150,
+                        align: 'center',
+                        render: (h, params) => {
+                            return h('div', [
+                                h('Button', {
+                                    props: {
+                                        type: 'primary',
+                                        size: 'small'
+                                    },
+                                    style: {
+                                        marginRight: '5px'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.show(params.index)
+                                        }
+                                    }
+                                }, '编辑'),
 
-}
-],
+                
+
+
+                                h('Button', {
+                                    props: {
+                                        type: 'error',
+                                        size: 'small'
+                                    },
+                                    on: {
+                                        click: () => {
+                                            this.remove(params.index)
+                                        }
+                                    }
+                                }, '删除'),
+
+
+
+
+                                        // props: {
+                                        //      type: 'error',
+                                        //      size: 'small'
+                                        // },
+
+                // h('Button',  [
+                //     h('Poptip', {
+                //         props: {
+                //             confirm: true,
+                //             title: '确定要删除吗！',
+                //             width: '150'
+                //         },
+                //         on: {
+                //              'on-ok': function(){
+                //                 alert('删除按钮，有效果')
+                //             },
+                //             'on-cancel': function(){
+                //                 alert('取消按钮，有效果')
+                //             }
+                //         }
+                //     }, '删除')
+                // ],)
+
+
+
+
+
+                            ]);
+                        }
+                    }
+                ],
+               
+
+
+            step: {
+                pass: '男'
+            },
+            account:'',
+            login_name:'',
+            height:'',
+            weight:'',
+            blue_name:'',
+            blue_address:'',
+            my_area:'',
+            companybox:[{value: '士明'}, {value: '馨靓'},  {value: '守利'},  {value: '冉然'},  {value: '彦宇'}],
+
+
+
+             step2: {
+                pass: '男'
+            },
+            account2:'',
+            login_name2:'',
+            height2:'',
+            weight2:'',
+            blue_name2:'',
+            blue_address2:'',
+            my_area2:'',
+            company2:'',
+
+
+
+            modal1: false,
+             modal2: false,
+          
             sexbox:[{value: '男'}, {value: '女'}, ],
             articleStateList: [{value: '果壳箱收集工'}, {value: '管理员'}, {value: '道路保洁工'}, {value: '果壳箱冲洗工'}, {value: '道路垃圾收集工'}],
             tableData: [],
@@ -214,14 +400,89 @@ export default {
         };
     },
     methods: {
-        handleChange(page) {
-            this.pageList.splice(0, this.pageList.length);
-            this.pageList=this.list.slice((page - 1) * 9,page * 9);
-        },
+
+           show (index) {
+
+                
+                 var aid =this.tableData[index].userid;
+xgid = this.tableData[index].userid;
+                axios.get('http://xhj.icecn.net/xhjapi/idStaff?session_id='+aa+'&id='+aid).then((res) => {
+
+                        var k=res.data;
+                        var vm = this;
+                
+
+                        vm.account2=k.account;
+                        vm.login_name2=k.login_name;
+                        vm.login_type2=k.login_type;
+                        vm.height2=k.height;
+                        vm.weight2=k.weight;
+                        vm.blue_name2=k.blue_name;
+                        vm.blue_address2=k.blue_address;
+                        vm.step2.pass=k.sex;
+                        vm.my_area2=k.my_area;
+                        vm.company2=k.company;
+
+                        vm.modal2=true;
+
+                    
+                });
+
+
+            },
+            ok2 () {
+
+                 axios.get('http://xhj.icecn.net/xhjapi/updateUser', {  
+                    params : { //请求参数
+                        id:xgid,
+                        session_id : aa,
+                        account : this.account2,
+                        login_name : this.login_name2,
+                        login_type : this.login_type2,
+                        height : this.height2,
+                        weight : this.weight2,
+                        blue_name : this.blue_name2,
+                        blue_address : this.blue_address2,
+                        sex : this.step2.pass,
+                        my_area : this.my_area2,
+                        company : this.company2
+                    }  
+                })
+                 .then((res) => {
+                    console.log(res.data);
+                    this.$Message.info('修改成功');
+                    this.getData();
+                });
+                
+            },
+            cancel2 () {
+                // this.$Message.info('Clicked cancel');
+            },
+
+            //删除
+            remove (index) {
+                var aid =this.tableData[index].userid;
+                axios.get('http://xhj.icecn.net/xhjapi/deleteUser', {  
+                    params : { //请求参数  
+                        id : aid,
+                    }  
+                }).then((res) => {
+                   this.tableData.splice(index, 1);
+                });   
+            },
+            
+           handlePage(value) {  
+            this.pageNum = value  
+            this.getData()  
+            },  
+            handlePageSize(value) {  
+            this.pageSize = value  
+            this.getData()  
+            },
             ok () {
                  axios.get('http://xhj.icecn.net/xhjapi/insertUser', {  
                     params : { //请求参数  
-                        date : s1,
+                        session_id : aa,
                         account : this.account,
                         login_name : this.login_name,
                         login_type : this.login_type,
@@ -244,33 +505,26 @@ export default {
                 // this.$Message.info('Clicked cancel');
             },
             handleDel (val, index, key) {
-                // alert(id)
+
                 
-                
-                // console.log(this.tableData[index]);
+                console.log("删除的id:",this.tableData[index].account);
             
             },
-            remove (index) {
-                 let self = this;
-                 this.$Modal.confirm({
-                    title: '用户信息',
-                    content: `是否删除此记录`,
-                    onOk: function(){
-                        this.$Loading.start();
-                        let para = {id: index}
-                         removeUser(para).then((res)=> {
-                            self.mockTableData();
-                      });
-                    }
-                })
-            },
+       
         getData () {
             // this.columnsList = tableData.table1Columns;
 
             //this.tableData = [{account:"",step:"数据加载中",heart:""}];
-                axios.get('http://xhj.icecn.net/xhjapi/listUser?date='+s1).then((res) => {
+
+            var xm =this.model1;
+            var ssxm =this.ssxm;
+            var sszh =this.sszh;
+                axios.get('http://xhj.icecn.net/xhjapi/listUser?session_id='+aa+'&company='+xm+'&account='+ssxm+'&login_name='+sszh+'&pageNum='+this.pageNum+'&pageSize='+this.pageSize).then((res) => {
 
                     var k=res.data.list;
+                    var vm = this;
+
+                    vm.pageTotal = parseInt(res.data.total); 
                     var json = [];
                     for(var i=0;i<k.length;i++){
                                 var j = {};
@@ -302,12 +556,14 @@ export default {
             
         },handleSearch3 () {
             
-            var xm =this.model1;
             
-
-                axios.get('http://xhj.icecn.net/xhjapi/listUser?date'+s1+'&company='+xm).then((res) => {
+            console.log('http://xhj.icecn.net/xhjapi/listUser?session_id='+aa+'&company='+xm+'&pageNum='+this.pageNum+'&pageSize='+this.pageSize);
+                axios.get('http://xhj.icecn.net/xhjapi/listUser?session_id='+aa+'&company='+xm+'&pageNum='+this.pageNum+'&pageSize='+this.pageSize).then((res) => {
 
                     var k=res.data.list;
+                    var vm = this;
+
+                    vm.pageTotal = parseInt(res.data.total); 
                     var json = [];
                     for(var i=0;i<k.length;i++){
                                 var j = {};
